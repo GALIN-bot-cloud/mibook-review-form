dfeedce09c75356edd7c1674e83aa37d9509b9b1
가린 <hlin0550@gmail.com>
2026-08-13 13:43:01 +0900

    신청폼 페이지 배경색 및 카드 색상 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-// [공개 페이지] 미북 앱 리뷰 작성자용 히어로(첫 화면)
-import PromoHero from "@/components/review/PromoHero";
-import SparkleDiamonds from "@/components/review/SparkleDiamonds";
+// [공개 페이지] 리뷰 인증 신청폼
+import ReviewForm from "@/components/ReviewForm";
 
-export default function ReviewFormPage() {
+export default function ApplyPage() {
   return (
-    <div className="page-bg">
-      <div className="heroStageGrid">
-        <SparkleDiamonds />
-        <div className="page-inner">
-          <PromoHero />
+    <div className="page-bg applyPageBg">
+      <div className="page-inner">
+        <div className="formSectionAnchor">
+          <ReviewForm />
         </div>
       </div>
     </div>
