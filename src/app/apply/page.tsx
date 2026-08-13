1cb2b467e3ddc4d37f71561b705b10b96655d03b
가린 <hlin0550@gmail.com>
2026-08-13 15:53:13 +0900

    신청폼 배경 클래스 복구

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -3,7 +3,7 @@ import ReviewForm from "@/components/ReviewForm";
 
 export default function ApplyPage() {
   return (
-    <div className="page-bg">
+    <div className="page-bg applyPageBg">
       <div className="page-inner">
         <div className="formSectionAnchor">
           <ReviewForm />
